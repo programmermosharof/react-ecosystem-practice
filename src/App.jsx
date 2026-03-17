9fa3449f8827cbb5571f5d4f967e04cd6c05be8b
programmermosharof <mosharof.dev@gmail.com>
2026-03-17 12:52:30 +0600

    Module 34 Learning Start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+
+function App() {
+
+  return (
+    <>
+    <header>
+       <DaisyNav></DaisyNav>
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+   
+  
+    </>
+  )
+}
+
+export default App
