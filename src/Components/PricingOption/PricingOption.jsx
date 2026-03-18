4010f04b85f2b9d57a5e8363051254062052568b
programmermosharof <mosharof.dev@gmail.com>
2026-03-18 17:08:38 +0600

    Pricing Card Added

--- src/Components/PricingOption/PricingOption.jsx
+++ src/Components/PricingOption/PricingOption.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCard from '../PricingCard/PricingCard';
+import DaisyPricingCard from '../DaisyPricing/DaisyPricingCard';
 
 
 
@@ -11,13 +12,22 @@ const PricingOption = ({pricingPromise}) => {
         <div>
             <h1 className='text-5xl text-center mb-6'>Join Our Pro Membership</h1>
             <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4'>
-                {
+                {/* {
                     pricing.map(pricing => <PricingCard
                     pricing={pricing}
                     key={pricing.id}
                     >
 
                     </PricingCard>)
+                } */}
+                {
+                    pricing.map(pricing => 
+                        <DaisyPricingCard 
+                        key={pricing.id}
+                        pricing={pricing}
+                        
+                        ></DaisyPricingCard>
+                    )
                 }
               
                
