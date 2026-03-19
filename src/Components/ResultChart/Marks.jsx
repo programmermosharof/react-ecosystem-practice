d20fd507e613194d0020bc09a17006b8a018b3c3
programmermosharof <mosharof.dev@gmail.com>
2026-03-19 12:52:24 +0600

    Bar chart added

--- src/Components/ResultChart/Marks.jsx
+++ src/Components/ResultChart/Marks.jsx
@@ -1,12 +1,35 @@
 import React, { use } from 'react';
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Marks = ({marksPromise}) => {
-    const marksData =use(marksPromise)
-    console.log(marksData);
+    const marksDataRes =use(marksPromise)
+    const marksData = marksDataRes.data;
+    const marksChartData = marksData.map(studentData => {
+        const student = {
+            id: studentData.student_id,
+            name: studentData.name,
+            math:studentData.marks.math,
+            bangla: studentData.marks.bangla,
+            english: studentData.marks.english
+        } 
+        const avg = (student.math + student.bangla + student.english) /3;
+        student.avg=avg.toFixed(2);
+        return student
+    })
+    console.log(marksChartData);
+    
     
     return (
-        <div>
-            
+        <div className='w-11/12 mx-auto'>
+            <BarChart width={1700}  data={marksChartData}>
+            <XAxis dataKey={"name"}></XAxis>
+            <YAxis></YAxis>
+            <Tooltip></Tooltip>
+            <Bar dataKey={"avg"} fill="#8884d8" ></Bar>
+            <Bar dataKey={"math"} fill='green'></Bar>
+            <Bar dataKey={"bangla"} fill='blue'></Bar>
+            <Bar dataKey={"english"} fill='purple'></Bar>
+            </BarChart>
         </div>
     );
 };
